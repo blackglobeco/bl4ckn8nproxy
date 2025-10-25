c1bc54baf4b1a2fa23b07c6516437fdb1ab58a78
Black Globe <137195478+blackglobeco@users.noreply.github.com>
2025-10-25 11:52:34 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,16 +5,23 @@ const proxy = createProxyMiddleware({
   changeOrigin: true,
   secure: false,
   onProxyRes: (proxyRes) => {
+    // Remove headers that block embedding
     delete proxyRes.headers["x-frame-options"];
     delete proxyRes.headers["content-security-policy"];
+    delete proxyRes.headers["cross-origin-opener-policy"];
+    delete proxyRes.headers["cross-origin-embedder-policy"];
+    delete proxyRes.headers["cross-origin-resource-policy"];
+
+    // Allow embedding everywhere
+    proxyRes.headers["access-control-allow-origin"] = "*";
+    proxyRes.headers["access-control-allow-headers"] = "*";
+    proxyRes.headers["access-control-allow-methods"] = "GET,POST,PUT,DELETE,OPTIONS";
   },
   pathRewrite: { "^/": "/" },
 });
 
 export default function handler(req, res) {
   proxy(req, res, (err) => {
-    if (err) {
-      res.status(500).send("Proxy error");
-    }
+    if (err) res.status(500).send("Proxy error");
   });
 }
